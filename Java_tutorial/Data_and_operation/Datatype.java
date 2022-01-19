3d337ad406c01dd15496d54fe516d0f4818163a6
Kingnamji <kingnamji@naver.com>
2022-01-19 20:43:14 +0900

    Tutorial Update

---
+++ Java_tutorial/Data_and_operation/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); // Number
+		System.out.println("six"); // String
+		System.out.println("6"); // String
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66
+		
+		System.out.println(6*6); // 36
+		//System.out.println("6"*"6"); Error
+		
+		System.out.println("1111".length()); // 4
+		// 자바도 마찬가지로 데이터에 타입이 있다. => 데이터 타입마다 연산을 달리
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Java_Study
+
+
+## Java Tutorial
+생활코딩, Java1 강의를 참고했습니다. (https://opentutorials.org/module/4294)
