b85ba700a16b2f3265e1dd7cdab01a0bdd22076f
Kingnamji <kingnamji@naver.com>
2022-01-23 22:36:12 +0900

    Tutorial Update

---
+++ Java_tutorial/Variables/Letter.java
@@ -0,0 +1,15 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "DY";
+		String Author = "Kingnamji";
+		
+		System.out.println("Hello " + name + " blahblah... From " + Author);
+		
+		double VAT = 9.99;
+		System.out.println(VAT);
+
+	}
+
+}
